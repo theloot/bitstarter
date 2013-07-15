a282e69575bc351a08a7ecf9bf834a0f04654cfa
Luthan Hill <luthanhill3@gmail.com>
2013-07-15 02:19:35 +0000

    Added buffer

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var buffer = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send(buffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
