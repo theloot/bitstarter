d2ba184401ed2961cda02cb87a864663f72d1ca3
Luthan Hill <luthanhill3@gmail.com>
2013-07-19 04:22:45 +0000

    Changed port to 8080 and removed buffer mods

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8'));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
